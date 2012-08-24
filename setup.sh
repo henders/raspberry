09e048e889dc02de8f5ba3d4651e282fde1304d3
Shane Hender <henders@gmail.com>
2012-08-24 11:23:21 -0700

    Updated for XBMC config

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-apt-get update
-apt-get upgrade
+apt-get -y update
+apt-get -y upgrade
 # Install vim and ruby/rake for the janus vim settings
-apt-get install vim rake
+apt-get -y install wget vim rake
 
 # Enable IPv6 so that a link-local address will show up on the network during headless operation
 echo ipv6 >> /etc/modules
@@ -12,3 +12,10 @@ cp wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 
 # Setup the janus configuration for VIM
 curl -Lo- https://bit.ly/janus-bootstrap | bash
+
+# Setup XBMC from pre-built source (save >24hours)
+apt-get -y install libmicrohttpd10 libsmbclient libavahi-client3  libjasper1 libyajl2 libtiff4 libfontconfig1 libfribidi0 libpng12-0 libcdio13 libsamplerate0 sqlite3 libcurl3-gnutls libtinyxml2.6.2 libssh-4 libmysqlclient18 liblzo2-2 libpcrecpp0
+wget http://www.memetic.org/files/xbmc.tar.gz
+tar zxvf xbmc.tar.gz
+cp -av xbmc/usr /
+cp /boot/arm128_start.elf /boot/start.elf
