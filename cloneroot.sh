0e2269f4e1670240b706c3a4a8fa6992fc9e888a
Shane Hender <henders@gmail.com>
2012-08-29 13:00:50 -0700

    Added cloneroot script, and modified vim setup

---
+++ cloneroot.sh
@@ -0,0 +1,5 @@
+mount /dev/sda1 /mnt
+time rsync -vxaHD --delete / /mnt/
+rsync -xaHD --delete /dev/ /mnt/dev/
+rm -rf /mnt/dev/.udevdb
+umount /dev/hda1
--- setup.sh
+++ setup.sh
@@ -11,8 +11,9 @@ apt-get -y install avahi-autoipd
 cp interfaces /etc/networks/interfaces
 cp wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 
-# Setup the janus configuration for VIM
-#curl -Lo- https://bit.ly/janus-bootstrap | bash
+# Setup the configuration for VIM
+git clone git://github.com/henders/vimrc.git
+bash vimrc/auto-install.sh
 
 # Setup XBMC from pre-built source (save >24hours)
 apt-get -y install libmicrohttpd10 libsmbclient libavahi-client3  libjasper1 libyajl2 libtiff4 libfontconfig1 libfribidi0 libpng12-0 libcdio13 libsamplerate0 sqlite3 libcurl3-gnutls libtinyxml2.6.2 libssh-4 libmysqlclient18 liblzo2-2 libpcrecpp0
